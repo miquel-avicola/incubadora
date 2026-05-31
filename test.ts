7f53e2eda0d5425dde34a58538138a857dcb97e4
Enric <enric.dlt@gmail.com>
2026-05-31 15:05:36 +0200

    Docs: previsio per estirp+edat+maquina i vocabulari; auditoria a fitxer propi; neteja de fitxers antics

--- test.ts
+++
@@ -1,8 +0,0 @@
-import { calcularPrevisioFinal } from './lib/previsio';
-
-async function main() {
-  const res = await calcularPrevisioFinal(2, 'Ross', 63, 'Multistage', '2026-05-26');
-  console.log(JSON.stringify(res, null, 2));
-}
-
-main().catch(console.error);
